ceb8dec98710b378ce4e6b36e6b1e0a0ee895915
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2022-01-27 11:45:34 +0100

    feat: add tests (#1)

--- core/src/main/scala/io/univalence/crier/Main.scala
+++ core/src/main/scala/io/univalence/crier/Main.scala
@@ -5,7 +5,6 @@ import sttp.client3.asynchttpclient.zio.{AsyncHttpClientZioBackend, SttpClient}
 import io.univalence.crier.Domain.{Post, PostProperties}
 import io.univalence.crier.Domain.PostStatus.{NotValid, Pending, Posted}
 import io.univalence.crier.Notion.{NotionApi, NotionApiLive}
-import io.univalence.crier.Validator.validatePage
 
 import zio._
 import zio.config._
@@ -46,14 +45,15 @@ object Main extends ZIOAppDefault {
   def sortPosts(pages: List[Post]): List[Post] =
     pages.sortBy(_.properties.createdTime).sortBy(_.properties.publicationDate.getOrElse(LocalDate.MAX))
 
-  /**
-   * Validate the pages according to our predicates and separate them
-   * between pending and other status.
-   */
-  def validateAndPartitionPosts(posts: List[Post]): (List[Post], List[Post]) =
-    posts.map(_.validate).partition(_.properties.status.getOrElse(NotValid) == Pending)
+  /** Separate posts between pending and other status. */
+  def partitionPosts(posts: List[Post]): (List[Post], List[Post]) =
+    posts.partition(_.properties.status.getOrElse(NotValid) == Pending)
 
-  /** Attribute publication date to valid pages. */
+  /**
+   * Attribute publication date to valid pages.
+   *
+   * Posts should be send from Monday to Friday
+   */
   def assignPublicationDates(posts: List[Post]): ZIO[Clock, Nothing, List[Post]] = {
     val sortedPosts = sortPosts(posts)
     val publicationDatesEffect =
@@ -76,12 +76,17 @@ object Main extends ZIOAppDefault {
     })
   }
 
-  def updateNotionDatabase: ZIO[Clock with Console with NotionApi, Throwable, List[Post]] =
+  /** Find the post to post today. */
+  def findTodayPost(posts: List[Post]): ZIO[Clock, Nothing, Option[Post]] =
+    Clock.localDateTime.map(now => posts.find(_.properties.publicationDate.contains(now.toLocalDate)))
+
+  def processNotionDatabase: ZIO[Clock with Console with NotionApi, Throwable, List[Post]] =
     for {
       database <- NotionApi(_.retrieveDatabase)
       postProperties = database.listOfProperties.filter(postShouldBeChecked)
       posts <- NotionApi(_.retrievePosts(postProperties))
-      (pendingPosts, notValidPosts)       = validateAndPartitionPosts(posts)
+      validatedPosts                      = posts.map(_.validate)
+      (pendingPosts, notValidPosts)       = partitionPosts(validatedPosts)
       notValidPostsWithoutPublicationDate = notValidPosts.map(_.withPublicationDate(None))
       _                               <- NotionApi(_.updatePosts(notValidPostsWithoutPublicationDate))
       pendingPostsWithPublicationDate <- assignPublicationDates(pendingPosts)
@@ -96,8 +101,8 @@ object Main extends ZIOAppDefault {
 
   def program: ZIO[Clock with Console with NotionApi, Throwable, Unit] =
     for {
-      pendingPages <- updateNotionDatabase
-      todayPage = pendingPages.headOption
+      pendingPages <- processNotionDatabase
+      todayPage    <- findTodayPost(pendingPages)
       _ <-
         todayPage match {
           case Some(page) => postPage(page)
--- core/src/test/scala/io/univalence/crier/MainTest.scala
+++ core/src/test/scala/io/univalence/crier/MainTest.scala
@@ -1,7 +1,143 @@
 package io.univalence.crier
 
+import io.univalence.crier.Domain.{Post, PostKind, PostProperties, PostStatus}
+import io.univalence.crier.Main.{assignPublicationDates, findTodayPost, partitionPosts, sortPosts}
+
 import zio.test._
+import zio.test.Assertion._
+
+import java.time.{LocalDate, ZonedDateTime, ZoneId}
 
 object MainTest extends DefaultRunnableSpec {
-  def spec: Spec[Annotations with Live, TestFailure[Any], TestSuccess] = ???
+  val timeZone: ZoneId             = ZoneId.of("UTC")
+  val zonedDateTime: ZonedDateTime = ZonedDateTime.of(2021, 10, 1, 0, 0, 0, 0, timeZone)
+
+  val fakePost: Post =
+    Post(
+      properties =
+        PostProperties(
+          id              = "id",
+          createdTime     = zonedDateTime,
+          kind            = Some(PostKind.Tips),
+          status          = Some(PostStatus.NotValid),
+          keywords        = List(),
+          publicationDate = None
+        ),
+      content = List()
+    )
+
+  def spec: Spec[Annotations with Live with TestClock, TestFailure[Any], TestSuccess] =
+    sortPostsSpec + partitionPostsSpec + assignPublicationDatesSpec + findTodayPostSpec
+
+  val sortPostsSpec: Spec[Any, TestFailure[Nothing], TestSuccess] =
+    suite("sortPosts Spec")(
+      test("sortPosts should sort by creation date if there is no publication date") {
+        val firstPost: Post  = fakePost.withCreatedTime(zonedDateTime.minusDays(1))
+        val secondPost: Post = fakePost
+
+        val posts: List[Post] = List(secondPost, firstPost)
+
+        assert(sortPosts(posts))(equalTo(List(firstPost, secondPost)))
+      },
+      test("sortPosts should prioritize some publication date over none") {
+        val firstPost: Post  = fakePost.withPublicationDate(Some(LocalDate.of(2022, 1, 1)))
+        val secondPost: Post = fakePost
+
+        val posts: List[Post] = List(secondPost, firstPost)
+
+        assert(sortPosts(posts))(equalTo(List(firstPost, secondPost)))
+      },
+      test("sortPosts should prioritize publication date over creation date") {
+        val firstPost: Post =
+          fakePost
+            .withPublicationDate(Some(LocalDate.of(2022, 1, 1)))
+        val secondPost: Post =
+          fakePost
+            .withPublicationDate(Some(LocalDate.of(2022, 1, 2)))
+            .withCreatedTime(zonedDateTime.minusDays(1))
+
+        val posts: List[Post] = List(secondPost, firstPost)
+
+        assert(sortPosts(posts))(equalTo(List(firstPost, secondPost)))
+      }
+    )
+
+  val partitionPostsSpec: Spec[Any, TestFailure[Nothing], TestSuccess] =
+    suite("partitionPosts Spec")(
+      test("partitionPosts should separate posts according to their status in two groups") {
+        val firstPost: Post  = fakePost.withStatus(PostStatus.NotValid)
+        val secondPost: Post = fakePost.withStatus(PostStatus.Pending)
+        val thirdPost: Post  = fakePost.withStatus(PostStatus.Posted)
+
+        val posts: List[Post] = List(secondPost, firstPost, thirdPost)
+
+        assert(partitionPosts(posts))(equalTo((List(secondPost), List(firstPost, thirdPost))))
+      }
+    )
+
+  val assignPublicationDatesSpec: Spec[TestClock, TestFailure[Nothing], TestSuccess] =
+    suite("assignPublicationDates Spec")(
+      test("assignPublicationDates should assign consecutive publication date") {
+        val posts: List[Post] = List(fakePost, fakePost)
+
+        val dateTimeReference = zonedDateTime.plusDays(3).toOffsetDateTime
+
+        // The 2021/10/4 is a Monday
+        for {
+          _             <- TestClock.setDateTime(dateTimeReference)
+          postsWithDate <- assignPublicationDates(posts)
+          dates = postsWithDate.map(_.properties.publicationDate.get)
+        } yield assert(dates)(equalTo(List(dateTimeReference.toLocalDate, dateTimeReference.toLocalDate.plusDays(1))))
+      },
+      test("assignPublicationDates should ignore saturday and sunday") {
+        val posts: List[Post] = List(fakePost, fakePost)
+
+        val dateTimeReference = zonedDateTime.toOffsetDateTime
+
+        // The 2021/10/1 is a Friday
+        for {
+          _             <- TestClock.setDateTime(dateTimeReference)
+          postsWithDate <- assignPublicationDates(posts)
+          dates = postsWithDate.map(_.properties.publicationDate.get)
+        } yield assert(dates)(equalTo(List(dateTimeReference.toLocalDate, dateTimeReference.toLocalDate.plusDays(3))))
+      },
+      test("assignPublicationDates can be launched on saturday or friday without issues") {
+        val posts: List[Post] = List(fakePost, fakePost)
+
+        val dateTimeReference = zonedDateTime.plusDays(1).toOffsetDateTime
+
+        // The 2021/10/2 is a Saturday
+        for {
+          _             <- TestClock.setDateTime(dateTimeReference)
+          postsWithDate <- assignPublicationDates(posts)
+          dates = postsWithDate.map(_.properties.publicationDate.get)
+        } yield assert(dates)(
+          equalTo(List(dateTimeReference.toLocalDate.plusDays(2), dateTimeReference.toLocalDate.plusDays(3)))
+        )
+      }
+    )
+
+  val findTodayPostSpec: Spec[TestClock, TestFailure[Nothing], TestSuccess] =
+    suite("findTodayPost Spec")(
+      test("findTodayPost should find a post if its publication date is today") {
+        val dateTimeReference = zonedDateTime.toOffsetDateTime
+
+        val posts: List[Post] = List(fakePost.withPublicationDate(Some(dateTimeReference.toLocalDate)))
+
+        for {
+          _         <- TestClock.setDateTime(dateTimeReference)
+          maybePost <- findTodayPost(posts)
+        } yield assert(maybePost)(isSome)
+      },
+      test("findTodayPost should not find a post if its publication date is not today") {
+        val dateTimeReference = zonedDateTime.toOffsetDateTime
+
+        val posts: List[Post] = List(fakePost)
+
+        for {
+          _         <- TestClock.setDateTime(dateTimeReference)
+          maybePost <- findTodayPost(posts)
+        } yield assert(maybePost)(isNone)
+      }
+    )
 }
